f121893d59a100435405f9a5a171a58522eef938
unique05 <perfectok05@gmail.com>
2025-01-16 15:03:42 +0100

    initial commit

---
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GamingConfiguration.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learn_spring_framework.game;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class GamingConfiguration {
+//
+//    @Bean
+//    public GamingConsole game(){
+//        var game = new PacmanGame();
+//        return game;
+//    }
+//
+//    @Bean
+//    public GameRunner gameRunner(GamingConsole game){
+//        var gameRunner = new GameRunner(game);
+//        return gameRunner;
+//    }
+//
+//}
